355f63297b2bc4409c449551660deba07b77cd7a
yxf <104021773@qq.com>
2017-10-24 15:55:40 +0800

    第一次提交

--- app/src/main/java/com/rongwei/yxfconclusionutils/aui/activity/rv/PullToRefreshUseActivity.java
+++ app/src/main/java/com/rongwei/yxfconclusionutils/aui/activity/rv/PullToRefreshUseActivity.java
@@ -1,9 +1,12 @@
 package com.rongwei.yxfconclusionutils.aui.activity.rv;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.rongwei.yxfconclusionutils.R;
@@ -18,6 +21,9 @@ import butterknife.ButterKnife;
 
 public class PullToRefreshUseActivity extends AppCompatActivity implements BaseQuickAdapter.RequestLoadMoreListener,SwipeRefreshLayout.OnRefreshListener{
 
+    public static final int FRESH_CODE=0;
+    public static final int LOAD_CODE=1;
+
     @Bind(R.id.rv_list)
     RecyclerView rvList;
     @Bind(R.id.swipeLayout)
@@ -25,30 +31,92 @@ public class PullToRefreshUseActivity extends AppCompatActivity implements BaseQ
     PullToRefreshAdapter adapter;
     private List<AnimationItem> data=new ArrayList<>();
 
+    private static final int TOTAL_COUNTER = 28;
+
+    private static final int PAGE_SIZE = 10;
+
+    private int delayMillis = 2000;
+
+    private int mCurrentCounter = 0;
+
+    private boolean isErr;//加载数据的时候发生错误(网络或其他错误)的时候
+    private boolean mLoadMoreEndGone = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pull_to_refresh_use);
         ButterKnife.bind(this);
-        initData();
         swipeLayout.setOnRefreshListener(this);
+        rvList.setLayoutManager(new LinearLayoutManager(this));
         adapter=new PullToRefreshAdapter(R.layout.list_item_animation,data);
-
-    }
-
-    private void initData() {
-        for (int i = 0; i < 40; i++) {
-            data.add(new AnimationItem("animation title"+i));
-        }
+        adapter.setEnableLoadMore(true);
+        adapter.setOnLoadMoreListener(this,rvList);
+        adapter.openLoadAnimation(BaseQuickAdapter.SCALEIN);
+        adapter.isFirstOnly(false);
+        rvList.setAdapter(adapter);
+        initData();
     }
 
     @Override
     public void onRefresh() {
-
+        initData();
     }
 
     @Override
     public void onLoadMoreRequested() {
+        swipeLayout.setEnabled(false);
+        if (adapter.getData().size() < PAGE_SIZE) {
+            adapter.setEnableLoadMore(true);
+        } else {
+            if (mCurrentCounter >= TOTAL_COUNTER) {
+                adapter.loadMoreEnd(mLoadMoreEndGone);//true is gone,false is visible
+            } else {
+                if (!isErr) {
+                   loadData();
+                } else {
+                    isErr = false;
+                    Toast.makeText(PullToRefreshUseActivity.this, R.string.network_err, Toast.LENGTH_LONG).show();
+                    adapter.loadMoreFail();
+                }
+            }
+        }
+        swipeLayout.setEnabled(true);
+    }
 
+    private void initData() {
+        adapter.setEnableLoadMore(false);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (data!=null) {
+                    data.clear();
+                }
+                for (int i = 0; i < PAGE_SIZE; i++) {
+                    data.add(new AnimationItem("animation title"+i));
+                }
+                adapter.setNewData(data);
+                isErr = false;
+                mCurrentCounter = PAGE_SIZE;
+                swipeLayout.setRefreshing(false);
+                adapter.setEnableLoadMore(true);
+            }
+        }, delayMillis);
+
+    }
+
+
+    private void loadData() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = mCurrentCounter; i < mCurrentCounter+PAGE_SIZE; i++) {
+                    data.add(new AnimationItem("animation title"+i));
+                }
+                adapter.setNewData(data);
+                mCurrentCounter = adapter.getData().size();
+                adapter.loadMoreComplete();
+            }
+        },delayMillis);
     }
 }
